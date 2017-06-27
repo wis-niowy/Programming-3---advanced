bd264d405de4c15b50edb4fb31bd8af1d8f2d4b2
Marcin Wiśniewski <marcin.w18@gmail.com>
2017-06-27 20:54:29 +0200

    Add files via upload

---
+++ lab11/lab11_2016a/IWatcher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11a
+{
+    class SimpleWatcher
+    {
+        public void Watch(IChangeNotifing arg)
+        {
+            arg.Changed += ServiceEvent;
+        }
+
+        public void StopWatching(IChangeNotifing arg)
+        {
+            arg.Changed -= ServiceEvent;
+        }
+
+        public void ServiceEvent(object sender, NotifyEventArgs args)
+        {
+            Console.WriteLine("Object changed");
+        }
+    }
+}
---
+++ lab11/lab11_2016a/SmartWatcher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11a
+{
+    class SmartWatcher
+    {
+        public void Watch(IChangeNotifing arg)
+        {
+            arg.Changed += ServiceEvent;
+        }
+
+        public void StopWatching(IChangeNotifing arg)
+        {
+            arg.Changed -= ServiceEvent;
+        }
+
+        public void ServiceEvent(object sender, NotifyEventArgs args)
+        {
+            var send = sender as IChangeNotifing;
+            Console.WriteLine("Object {0} changed property: {1}", send.Name, args.PropertyName);
+        }
+
+    }
+}
