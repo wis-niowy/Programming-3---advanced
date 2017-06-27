535f9f5c9897f81284f6e0cae12fb857b27ce9fb
Marcin Wiśniewski <marcin.w18@gmail.com>
2017-06-27 20:17:38 +0200

    Add files via upload

---
+++ lab5/lab5_2014/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PO_Lab5
+{
+	class Program
+	{
+        static void Main()
+        {
+            Zwierze[] zwierzeta = { new Pies("Burek"), new Kot("Puszek"), new Pies("Reksio"), };
+
+            // Etap 1 (2p.)
+            Console.WriteLine("\nZwierzęta mówią:");
+            for (int i = 0; i < zwierzeta.Length; i++)
+            {
+                zwierzeta[i].DajGlos();
+            }
+
+            // Etap 2 (1p.)
+            Console.WriteLine("\nZwierzęta liżą łapę:");
+            for (int i = 0; i < zwierzeta.Length; i++)
+            {
+                zwierzeta[i].Jedz();
+            }
+
+            // Etap 3 (2p.)
+            Jedzenie[] jedzenie = { new Kosc(), new Stek() };
+
+            Console.WriteLine("\nZwierzęta jedzą:");
+            for (int i = 0; i < zwierzeta.Length; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < jedzenie.Length; j++)
+                {
+                    zwierzeta[i].Jedz(jedzenie[j]);
+                }
+            }
+
+            Console.WriteLine();
+		}
+	}
+}
